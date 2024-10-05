17461478c892a125f3bd43892977fbb0da8921ee
Margret Riegert <margret@eowyn.net>
2024-10-05 03:09:59 -0400

    Skip monkey-patch if 1.14> Ignore direnv stuff

--- src/lsprotocol/uri.cr
+++ src/lsprotocol/uri.cr
@@ -1,3 +1,6 @@
+# https://github.com/crystal-lang/crystal/pull/14834
+{% skip_file if compare_versions(Crystal::VERSION, "1.14.0") >= 0 %}
+
 class URI
   def self.from_json_object_key?(key : String) : URI?
     parse key
